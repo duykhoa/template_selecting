dfca7378caae1433ee9792364d88988c3c0d582f
Duy Khoa <duykhoa12t@gmail.com>
2014-12-26 20:36:08 +0700

    migrate database for product migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20141226133140) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "index"
+    t.string   "create"
+    t.string   "update"
+    t.string   "destroy"
+    t.string   "edit"
+    t.string   "show"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
